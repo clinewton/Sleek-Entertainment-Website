efd85ea67d9eb3fa41081a7e8b72d25d74cf9129
Stephanie-Kibet <54350971+Stephanie-Kibet@users.noreply.github.com>
2019-10-28 10:56:27 +0300

    Add files via upload

---
+++ application/views/user/movie.php
@@ -0,0 +1,101 @@
+<html>
+<head>
+<title>Sleek Entertainment</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" href="css/style2.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body style="background-image: linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)), url(images/spider.jpg);" >
+<div class="container header">
+	<img src= "images/logo.png" class="logo">
+	<a href="login.php"><i class="fa fa-user"></i>Log In</a>
+	<a href="cart.php" class="cart-btn"><i class="fa fa-shopping-cart"></i>View Cart</a>
+
+</div>
+<div class="container movie-details">
+<div class="row">
+<div class="col-md-6 left-box">
+	<br>
+	<br>
+	<br>
+	<br>
+	<h1>Spiderman : Far From Home</h1>
+	<p>Subscribe to this website to book and pay for movies and series online.<br>Only a CLICK away!!</p>
+	<a href="register.php" class="signup-btn"><i class="fa fa-user"></i>Sign up</a>
+	<h5>Storyline: </h5>
+	<p>Our friendly neighborhood Super Hero decides to join his best friends Ned, MJ, and the rest of the gang on a European vacation. However, Peter's plan to leave super heroics behind for a few weeks are quickly scrapped when he begrudgingly agrees to help Nick Fury uncover the mystery of several elemental creature attacks, creating havoc across the continent. Written by Sony Pictures Entertainment</p>
+	<h5>Genres:</h5>
+	<p>Action |  Adventure | Sci-Fiction</p>
+	<h5>Release Date:</h5>
+	<p>2 July 2019 (USA)</p>
+
+	
+	<h5>Directed by: </h5> 
+<p>Jon Watts</p>	
+<h5>Writing Credits: </h5>  
+<p>Chris McKenna	...	(written by) &</p>
+<p>Erik Sommers	...	(written by)</p>
+ 
+<p>Stan Lee	...	(based on the Marvel comic book by) and</p>
+
+<p>Steve Ditko	...	(based on the Marvel comic book by)</p>
+	<a href="#"class="book-btn"><i class="fa fa-bell"></i>Add to Must Watch</a>
+	<a href="car.php"><i class="fa fa-shopping-cart"></i>Add to Cart</a>
+	<h5>Cast: </h5>
+	<p>Tom Holland, Zendaya, Jake Gyllenhaal, Jacob Batalon, Samuel L. Jackson, Marisa Tomei</p>
+
+
+	<div class="casting">
+		<img src="images/TomH.jpg" title="Tom Holland">
+		<img src="images/Zendaya.jpg" title="Zendaya">
+		<img src="images/JakeGy.jpg" title="Jake Gyllenhaal">
+		<img src="images/JacobBatalon.jpg" title="Jacob Batalon">
+		<img src="images/SamuelJackson.jpg" title="Samuel L. Jackson">
+		<img src="images/MarisaTomei.jpg" title="Marisa Tomei">
+</div>
+
+
+</div>
+<div class="col-md-6 text-center">
+	<img src="images/spiderman.jpg" class="movie-img">
+	<br><br>
+<div class="video">
+	<p>Not convinced? Watch the Trailer BELOW!! 
+<b href="https://www.youtube.com/watch?v=Nt9L1jCKGnE" target="_blank"></b>
+</p>
+<video controls>
+  <source src="images/spiderman.mov" type="video/mov">
+  Your browser does not support HTML5 video.
+</video>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="container series">
+	<h3>Latest HD Movie Releases</h3>	
+<div class="row">
+<div class="col-md-4">
+	<div class="row">
+		<div class="col-6"><img src="images/wick3.jpg"></div>
+		<div class="col-6"><img src="images/cpmarvel.jpg"></div>	
+	</div>
+</div>
+<div class="col-md-4">
+	<div class="row">
+		<div class="col-6"><img src="images/endgame.jpg"></div>
+		<div class="col-6"><img src="images/toystory.jpg"></div>	
+	</div>
+</div>
+<div class="col-md-4">
+	<div class="row">
+		<div class="col-6"><img src="images/aladdin.jpg"></div>
+		<div class="col-6"><img src="images/joker.jpg"></div>	
+	</div>
+</div>
+</div>
+</div>
+</body>
+</html>
